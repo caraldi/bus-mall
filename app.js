dac0b4e0e4277b4595a2abe941bf504d293bdf3b
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-29 12:14:59 -0700

    updated files after class demo

---
+++ app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var imagePaths = ['bag.jpg', 'banana.jpg', 'scissors.jpg', 'pen.jpg'];
+var images = [];
+
+// Looping through imagePaths array to create new Image object for each, which pushes to images array
+for (var i = 0; i < imagePaths.length; i++) {
+  var name = imagePaths[i];
+  new Image(null, name);
+}
+console.log(imagePaths, images);
+
+// Create elements
+var imageList = document.getElementById('images');
+
+imageList.addEventListener('click', clickHandler);
+
+drawImage();
+drawImage();
+drawImage();
+
+// Create clickHandler function for event 'click' which calls on drawImage function
+function clickHandler(e) {
+// Clear list
+  console.log(e.target);
+// Use event target to determine which image was clicked (loop through and compare paths of each)
+// Add to the views of all images displayed
+// Add to clicks of just the clicked image
+  imageList.textContent = '';
+  drawImage();
+  drawImage();
+  drawImage();
+}
+
+// Needs to eventually draw out all three
+function drawImage() {
+// Use the image path for the src
+// (image.path)
+    var img = document.createElement('img');
+    var li = document.createElement('li');
+    var imageList = document.getElementById('images');
+    var randomIndex = Math.floor(Math.random() * imagePaths.length);
+    var randomPath = imagePaths[randomIndex];
+
+// Set src
+    img.setAttribute('src', 'imgs/' + randomPath);
+
+// Add to DOM
+    li.appendChild(img);
+    imageList.appendChild(li);
+}
+
+function Image(name, path) {
+  this.views = 0;
+  this.clicks = 0;
+  this.name = name;
+  this.path = path;
+
+  images.push(this);
+}
