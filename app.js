7d66111a240b1f5e3e6b7310e7bf0ed1e8c482ed
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-29 17:19:42 -0700

    added some functionality tests

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var imagePaths = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+var imagePaths = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 var images = [];
 
 // Looping through imagePaths array to create new Image object for each, which pushes to images array
@@ -21,11 +21,30 @@ drawImage();
 
 // Create clickHandler function for event 'click' which calls on drawImage function
 function clickHandler(e) {
-// Clear list
-  console.log(e.target);
+  console.log('Target clicked:', e.target);
+
 // Use event target to determine which image was clicked (loop through and compare paths of each)
+function findObject() {
+  for (var j = 0; j < images.length; j++) {
+    var match = 'sweep.png';
+    if (match === images[j].path) {
+      function addClick() {
+
+      };
+  } else {
+    console.log('No match');
+    }
+  }
+}
+findObject();
+
 // Add to the views of all images displayed
+
+
 // Add to clicks of just the clicked image
+
+
+// Reset imageList;
   imageList.textContent = '';
   drawImage();
   drawImage();
@@ -39,8 +58,9 @@ function drawImage() {
     var img = document.createElement('img');
     var li = document.createElement('li');
     var imageList = document.getElementById('images');
-    var randomIndex = Math.floor(Math.random() * imagePaths.length);
-    var randomPath = imagePaths[randomIndex];
+    var randomIndex = Math.floor(Math.random() * images.length);
+    var randomPath = images[randomIndex].path;
+    console.log('Random path:', randomPath);
 
 // Set src
     img.setAttribute('src', 'imgs/' + randomPath);
