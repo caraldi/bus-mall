5470f2ecdd6175e6ddf49a4b33ae0451b676be85
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-31 16:50:45 -0700

    broke things and fixed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Begin code dealing with Image objects display
-
 var imagePaths = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 var images = [];
 var currentImageIndices = [0, 1, 2];
@@ -19,8 +18,6 @@ function Image(name, path) {
 
 // Loop through imagePaths array to create new Image object for each image
 for (var i = 0; i < imagePaths.length; i++) {
-
-// Use split to remove path for name variable
   var name = imagePaths[i].split('.')[0];
   var path = imagePaths[i];
 
@@ -29,19 +26,17 @@ for (var i = 0; i < imagePaths.length; i++) {
 
 // Create function to draw images based on object path
 function drawImage(index) {
-
-// Use the image path for the src
-    var img = document.createElement('img');
-    var li = document.createElement('li');
-    var imageList = document.getElementById('images');
-    var randomPath = images[index].path;
+  var img = document.createElement('img');
+  var li = document.createElement('li');
+  var imageList = document.getElementById('images');
+  var randomPath = images[index].path;
 
 // Set src
-    img.setAttribute('src', randomPath);
+  img.setAttribute('src', randomPath);
 
 // Add to DOM
-    li.appendChild(img);
-    imageList.appendChild(li);
+  li.appendChild(img);
+  imageList.appendChild(li);
 }
 
 // Create ul of images
@@ -76,57 +71,14 @@ function clickHandler(event) {
   }
   currentImageIndices = arrayOfRandomIndices;
 
-// Reset imageList;
-    imageList.textContent = '';
-    drawImage(arrayOfRandomIndices[0]);
-    drawImage(arrayOfRandomIndices[1]);
-    drawImage(arrayOfRandomIndices[2]);
-
-// End of code dealing with Image objects display
-
-// Begin code dealing with display of data in chart
-var chartButton = document.getElementById('show_chart');
-chartButton.addEventListener('click', chartClickHandler);
-
-// Create two arrays to add image names as labels to chart and tracked clicks as bar data
-var imageNames = [];
-var imageClicks = [];
-
-// Push names and clicks to imageNames and imageClicks arrays
-    for (i = 0; i < images.length; i++) {
-      imageNames.push(images[i].name);
-      imageClicks.push(images[i].clicks);
-      }
-
-    // Create chartClickHandler function to display imageClicks as votes
-    // Needs to be placed in event handler function
-    function chartClickHandler() {
-
-    }
-
-    // Create chart using charjs library
-    var ctx = document.getElementById('chart_canvas');
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: imageNames,
-        datasets: [{
-          data: imageClicks
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+// Reset imageList
+imageList.textContent = '';
+drawImage(arrayOfRandomIndices[0]);
+drawImage(arrayOfRandomIndices[1]);
+drawImage(arrayOfRandomIndices[2]);
 }
 
-// Create function to create randomized index
+// Create function for generation of randomized image list displays
 function randomIndex() {
   return Math.floor(Math.random() * images.length);
 }
@@ -148,3 +100,47 @@ function randomIndices() {
   }
   return [firstRandomIndex, secondRandomIndex, thirdRandomIndex];
 }
+
+// End of code dealing with Image objects display
+
+
+// Begin code dealing with display of data in chart
+var chartButton = document.getElementById('show_chart');
+chartButton.addEventListener('click', chartClickHandler);
+
+// Create two arrays to add image names as labels to chart and tracked clicks as bar data
+var imageNames = [];
+var imageClicks = [];
+
+// Push names and clicks to imageNames and imageClicks arrays
+for (i = 0; i < images.length; i++) {
+  imageNames.push(images[i].name);
+  imageClicks.push(images[i].clicks);
+}
+
+// Create chartClickHandler function to display clicked images data
+// Needs to be placed in event handler function
+function chartClickHandler(event) {
+
+}
+
+// Create chart using charjs library
+var ctx = document.getElementById('chart_canvas');
+  new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: imageNames,
+        datasets: [{
+          data: imageClicks
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
